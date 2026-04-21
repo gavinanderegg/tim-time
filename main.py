bde261ba070cf8f0375e5a46a8da13b21810097b
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2026-04-21 09:55:20 -0300

    Stock values

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from tim-time!")
+import yfinance as yf
+import csv
 
+ticker = yf.Ticker('AAPL')
+data = ticker.history(start='2011-08-24', end='2026-04-20')
 
-if __name__ == "__main__":
-    main()
+with open('values.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['date', 'high'])
+    for dt, row in data.iterrows():
+        writer.writerow([dt.strftime('%Y-%m-%d'), row['High']])
+
